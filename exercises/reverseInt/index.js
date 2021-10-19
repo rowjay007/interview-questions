8636f466c4e09be13cf42aaafe9ac6a5dae5aa6a
rowjay007 <rowlives@gmail.com>
2021-10-19 11:38:09 +0100

    creating two working solution for integer reversal

---
+++ exercises/reverseInt/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {
+  return parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n);
+}
+
+// const reverseInt = (n) =>
+//   parseFloat(n.toString().split("").reverse().join("")) * Math.sign(n);
+
+module.exports = reverseInt;
